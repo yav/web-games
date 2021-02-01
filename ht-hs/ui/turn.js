6c0cf7230e6f512da43ae20ad082db1acb6734f8
Iavor Diatchki <iavor.diatchki@gmail.com>
2021-01-31 22:41:14 -0800

    Add some scoring help

--- ht-hs/ui/turn.js
+++ ht-hs/ui/turn.js
@@ -27,6 +27,41 @@ function drawTurn(turn) {
   dom.classList.add('turn')
   dom.style.fontSize = gui.board.fontSize
 
+  { // scoring help
+    const el = document.createElement('div')
+    el.classList.add('undo')
+    el.classList.add('question')
+    el.textContent = '?'
+    dom.appendChild(el)
+    gui.tooltip(el,'Scoring Help')
+
+    const help = document.createElement('div')
+    help.classList.add('score')
+    help.classList.add('help')
+    help.style.display = 'none'
+    const entry = function(l) {
+      const x = document.createElement('div')
+      x.innerHTML = l
+      help.appendChild(x)
+    }
+    entry('<b><u>End Game Scoring</u></b>')
+    entry('4 VP per max upgrade (not Keys)')
+    entry('#Bonuses: 1,3,3,6,6,10,10,15,15,21 VP')
+    entry('Workers on special action: 7,8,9,11 VP')
+    entry('2 VP per controlled city')
+    entry('Keys &#215; #offices in largest network')
+    entry('Most controlled cities in a province<br>(#offices break ties)')
+    dom.appendChild(help)
+
+    let visible = false
+
+    el.addEventListener('click',function() {
+      help.style.display = visible ? 'none' : 'inline-block'
+      visible = !visible
+    })
+
+  }
+
   { // undo btn
     const btn = document.createElement('span')
     btn.classList.add('question')
