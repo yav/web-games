ea144797d2431a82b2acc01708cf9e7d5cb67146
Iavor Diatchki <iavor.diatchki@gmail.com>
2021-05-29 10:28:54 -0700

    Checkpoint

--- anno/src/Basics.hs
+++ anno/src/Basics.hs
@@ -1,5 +1,8 @@
 module Basics where
 
+import Data.Map(Map)
+
+import Common.Basics
 import Common.Bag
 
 data Profession = Farmer | Worker | Craftsman | Engineer | Investor
@@ -20,9 +23,11 @@ data Resource   =
   | Soap | Windows | Goods | Glasses | Watch | Bulb | Gramophone
   | Dynamite | Gun | Artillery
   | SteamEngine | Bicycle | SteamCarriege
-  | ShipSpace ShipType
+  | ShipToken ShipType
   | Specialist Profession
-  | Dock Int
+
+  -- Just for buildings
+  | DockBuilding Int | ShipBuilding Ship
     deriving (Eq,Ord,Show)
 
 type Cost = Bag Resource
@@ -33,23 +38,91 @@ data Loc = Loc Int Int
 data Ship = Ship
   { shipLevel :: Int
   , shipType  :: ShipType
-  }
+  } deriving (Eq,Ord,Show)
 
 newtype NewWorld = NewWorld [ Resource ]
 data OldWorld = OldWorld  -- XXX: bonus
+data Expedition = Expedition
+data Goal = Goal
+data Extra = Extra
 
-data Building = Building
-  { producerCost    :: Maybe Cost
-  , producerOutput  :: Resource
-  , producerWorker  :: Profession
-  , _producerSpots  :: [Maybe Profession]
+--------------------------------------------------------------------------------
+data Card = Card
+  { cardCost :: Cost
+  , cardBenefit :: Benefit
   }
+
+data Benefit = Gain Int Resource
+             | Upgrade3 [Profession]
+             | Discard2
+             | GainExpedition2
+             | GainAction
+
+
+--------------------------------------------------------------------------------
+data Building = Building
+  { cost        :: Maybe Cost
+  , output      :: Resource
+  , profession  :: Maybe Profession
+  , _spots      :: [Maybe Profession]
+  }
+
+startBuilding :: Profession -> Resource -> Building
+startBuilding prof out = Building
+  { cost        = Nothing
+  , output      = out
+  , profession  = Just prof
+  , _spots      = [ Nothing, Nothing ]
+  }
+
+building :: Profession -> [Resource] -> Resource -> Building
+building prof co out = Building
+  { cost = Just (bagFromList co)
+  , output = out
+  , profession = Just prof
+  , _spots = [ Nothing, Nothing ]
+  }
+
+dock :: Int -> [Resource] -> Building
+dock lvl co = Building
+  { cost = Just (bagFromList co)
+  , output = DockBuilding lvl
+  , profession = Nothing
+  , _spots = []
+  }
+
+ship :: [Resource] -> Int -> ShipType -> Building
+ship co lvl ty = Building
+  { cost = Just (bagFromList co)
+  , output = ShipBuilding Ship { shipLevel = lvl, shipType = ty }
+  , profession = Nothing
+  , _spots = []
+  }
+--------------------------------------------------------------------------------
+
 
 data PlayerState = PlayerState
-  { _playerShips    :: [Ship]
-  , _playerShipping :: Bag ShipType
-  , _playerOldWorld :: [OldWorld]     -- old world
-  , _playerNewWorld :: [NewWorld]     -- how many and which
+  { _playerShips      :: [Ship]
+  , _playerShipping   :: Bag ShipType   -- available resource tokens
+  , _playerOldWorld   :: [OldWorld]     -- old world
+  , _playerNewWorld   :: [NewWorld]     -- how many and which
+  , _playerProduction :: Map Loc Building
+  , _playerWorkers    :: Bag Profession
+  , _playerGold       :: Int
+  , _playerHand       :: [Card]
+  , _playerCompleted  :: [Card]
+  , _playerExpedition :: [Expedition]
   }
 
-
+data Game = Game
+  { _turnOrder      :: [PlayerId]
+  , _players        :: Map PlayerId PlayerState
+  , goals           :: [Goal]
+  , freeAtions      :: [Extra]
+  , _deck3          :: [Card]
+  , _deck5          :: [Card]
+  , _deck8          :: [Card]
+  , _deckExpedition :: [Expedition]
+  , _market         :: [(Building,Int)]
+  , _supply         :: Bag Profession
+  }
