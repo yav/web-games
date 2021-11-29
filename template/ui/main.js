b75ecc1f379a21fbae3ddb858e75b661dedc72ce
Iavor Diatchki <iavor.diatchki@gmail.com>
2021-11-28 21:36:55 -0800

    Comments + missing placeholder function

--- template/ui/main.js
+++ template/ui/main.js
@@ -10,14 +10,21 @@ const main = () => {
   if (conn.size) iconSize = conn.size
 }
 
+// Redraw the whole state
 const uiRedraw = (state) => {
   uiQuestions(state.questions)
 }
 
+// Set the explanation for what we are asking.
+const uiSetQuestion = (q) => {
+}
+
+// Various things that can be used to answer the question.
 const uiQuestion = (q) => hsInput({
   })(q.chChoice)
 
 
+// Perform a partial update
 const uiUpdate = hsUpdate({
   })
 
